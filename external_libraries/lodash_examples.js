3dbf74aed81f9c40bb810490b8121e829ed785f9
jordanhudgens <jordan@devcamp.com>
2019-07-15 14:25:44 -0700

    Updated the versions of lodashb to fix dependency security alert

--- external_libraries/lodash_examples.js
+++ external_libraries/lodash_examples.js
@@ -1,6 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var _ = require("lodash");
+// Yep
+// const nums = [1, 2, 3];
+// console.log(_.last(nums));
+// Nope
+// const number = 10;
+// console.log(_.last(number));
 var playerNames = [
     "Springer",
     "Bregman",
@@ -13,4 +19,4 @@ var playerNames = [
     "Reddick"
 ];
 var lineupOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(_.zip(lineupOrder, playerNames));
+_.zip(lineupOrder, playerNames);
